e3e8f1059877d8b10ac88a4372267b6b9c12bc24
Jorge Rodríguez García <jorger09@ucm.es>
2018-11-27 21:07:09 +0100

    añadido ejercicio 57 (da wrong answer)

---
+++ Problemas del juez automático/Ej57/PlantillaJuezCentinela.cpp
@@ -0,0 +1,95 @@
+// Jorge Rodriguez Garcia
+// VJ24
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void cogeCandidato(int& minTrabajos, int&actual, pair<int, int>& candidato, pair<int, int>& trabajo) {
+	minTrabajos++;
+	actual = candidato.second;
+	candidato = trabajo;
+}
+
+// función que resuelve el problema
+void resolver(vector<pair<int, int>>& trabajos, const int& C, const int& F) {
+	int minTrabajos = 0;
+	int actual = C;
+	bool imposible = false;
+	pair<int, int> candidato = trabajos[0];  // candidato a considerar
+
+	if (trabajos.size() == 1) {             // si solo hay un trabajo y abarca todo el tiempo la solucion sera coger ese
+		if (candidato.first <= C && candidato.second >= F)cout << 1 << endl;
+		else cout << "Imposible" << endl;   
+	}
+	else {
+		int i = 1;
+
+		if (candidato.first > C)imposible = true;
+
+		while (i < trabajos.size() && actual < F && !imposible) {
+			if (candidato.first <= actual) {
+				if (trabajos[i].first <= actual) {
+					if (candidato.second > trabajos[i].second) cogeCandidato(minTrabajos, actual, candidato, trabajos[i]);
+					else candidato = trabajos[i];
+				}
+				else cogeCandidato(minTrabajos, actual, candidato, trabajos[i]);
+			}
+			else imposible = true;
+			i++;
+		}
+
+		if (imposible || actual < F)cout << "Imposible" << endl;
+		else cout << minTrabajos << endl;
+	}
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	int C, F, N;
+	cin >> C >> F >> N;
+    
+    if (C==0 && F==0 && N==0)
+        return false;
+
+	vector<pair<int, int>> trabajos;
+	for (int i = 0; i < N; i++) {
+		int comienzo, final;
+		cin >> comienzo >> final;
+		trabajos.push_back({ comienzo, final });
+	}
+
+	sort(trabajos.begin(), trabajos.end());
+    
+	if (N==0)cout << "Imposible" << endl;
+    else resolver(trabajos, C, F);
+    
+    return true;
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+    #ifndef DOMJUDGE
+     std::ifstream in("datos.txt");
+     auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+     #endif 
+    
+    
+    while (resuelveCaso())
+        ;
+
+    
+    // Para restablecer entrada. Comentar para acepta el reto
+     #ifndef DOMJUDGE // para dejar todo como estaba al principio
+     std::cin.rdbuf(cinbuf);
+     system("PAUSE");
+     #endif
+    
+    return 0;
+}
