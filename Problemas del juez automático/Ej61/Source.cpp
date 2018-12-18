a4bf84750324153fc511e9614d69b47ec6794d36
Jorge Rodríguez García <jorger09@ucm.es>
2018-12-18 20:47:21 +0100

    correct de ejercicios 61 y 68

---
+++ Problemas del juez automático/Ej61/Source.cpp
@@ -0,0 +1,96 @@
+﻿// Jorge Rodriguez Garcia
+// VJ24
+
+#include "Matriz.h"
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// calcula el maximo de oro que se puede coger de los cofres del vector "cofres", en el tiempo T
+int maxOro(const vector<pair<int, int>>& cofres, const int& T, Matriz<int>& matriz) {
+	int n = cofres.size() - 1;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= T; j++) {
+			if (3 * cofres[i].first > j)matriz[i][j] = matriz[i - 1][j]; // si no te da tiempo a bajar a por el, no lo coges (tardas 1 profundidad en bajar y 2 en subir)
+			else matriz[i][j] = max(matriz[i - 1][j], matriz[i - 1][j - 3 * cofres[i].first] + cofres[i].second); // si no, el maximo resultado de cogerlo o no cogerlo
+		}
+	}
+	return matriz[n][T];
+}
+
+// devuelve un vector con las posiciones de los cofres que he cogido
+vector<int> calcularCofresCogidos(const vector<pair<int, int>>& cofres, const int& T, const Matriz<int>& matriz) {
+	int n = cofres.size() - 1;
+	vector<int> cualesCofres;                   // guarda las posiciones de los cofres que he cogido
+	int i = n; int j = T;
+
+	for (int i = n; i >= 1; i--)
+	{
+		if (matriz[i][j] != matriz[i - 1][j]) { // si lo he cogido
+			cualesCofres.push_back(i);          // añado su posicion al vector
+			j = j - 3 * cofres[i].first;
+		}
+	}
+
+	return cualesCofres;
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	int T, N;
+	cin >> T >> N;
+
+	if (!std::cin)
+		return false;
+
+	vector<pair<int, int>> cofres;
+	cofres.push_back({ 0, 0 }); // porque luego se empieza a recorrer desde el 1
+	for (int i = 0; i < N; i++) {
+		int profundidad, oro;
+		cin >> profundidad >> oro;
+		cofres.push_back({ profundidad, oro });
+	}
+
+	int n = cofres.size() - 1;
+	Matriz<int> matriz(n + 1, T + 1, 0);
+	int maxOroCogido = maxOro(cofres, T, matriz);
+	vector<int> cualesCofres = calcularCofresCogidos(cofres, T, matriz);
+	int cuantosCofres = cualesCofres.size();
+
+	cout << maxOroCogido << endl << cuantosCofres << endl;
+
+	for (int i = cualesCofres.size() - 1; i >= 0; i--) {
+		cout << cofres[cualesCofres[i]].first << " " << cofres[cualesCofres[i]].second << endl;
+	}
+	cout << "----" << endl;
+
+	return true;
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	while (resuelveCaso())
+		;
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
---
+++ Problemas del juez automático/Ej68/PlantillaJuezCasosIlimitados.cpp
@@ -0,0 +1,87 @@
+// Jorge Rodriguez Garcia
+// VJ24
+
+#include "Matriz.h"
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const int INF = 1e9;
+
+// devuelve el numero minimo de monedas de los tipos del vector de monedas,
+// teniendo en cuenta la cantidad de cada tipo, para pagar un precio P
+int minMonedas(const vector<pair<int, int>>& monedas, const int& P) {
+	int n = monedas.size() - 1;
+	Matriz<int> tabla(n + 1, P + 1, INF);  // minMonedas para pagar P con 0 monedas->imposible
+	tabla[0][0] = 0;                       // minMonedas para pagar 0
+
+	for (int i = 1; i <= n; i++) {
+		tabla[i][0] = 0;                   // minMonedas para pagar 0
+		for (int j = 1; j <= P; j++) {
+			int minimo = tabla[i - 1][j];  // equivale a k = 0
+			for (int k = 1; k <= monedas[i].second && k*monedas[i].first <= j; k++) {
+				minimo = min(minimo, tabla[i - 1][j - k * monedas[i].first] + k);
+			}
+			tabla[i][j] = minimo;
+		}
+	}
+    
+	return tabla[n][P];
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	int N, precio;
+	cin >> N;
+
+    if (! std::cin)
+        return false;
+
+	vector<pair<int, int>> monedas;
+	monedas.push_back({ 0, 0 }); // NO OLVIDAR QUE EMPIEZA EN 1
+	for (int i = 0; i < N; i++) {
+		int valor;
+		cin >> valor;
+		monedas.push_back({ valor, 0 });
+	}
+	for (int i = 1; i <= N; i++) {
+		int cantidad;
+		cin >> cantidad;
+		monedas[i].second = cantidad;
+	}
+
+	cin >> precio;
+
+	int numMin = minMonedas(monedas, precio);
+	if (numMin == INF)cout << "NO" << endl;
+	else cout << "SI " << numMin << endl;
+    
+    return true;
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+    #ifndef DOMJUDGE
+     std::ifstream in("datos.txt");
+     auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+     #endif 
+    
+    
+    while (resuelveCaso())
+        ;
+
+    
+    // Para restablecer entrada. Comentar para acepta el reto
+     #ifndef DOMJUDGE // para dejar todo como estaba al principio
+     std::cin.rdbuf(cinbuf);
+     system("PAUSE");
+     #endif
+    
+    return 0;
+}
